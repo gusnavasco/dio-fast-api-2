3c6e18414bb2b5165a4a5175f4ca4a3f3ec15672
gusnavasco <gusnavasco@gmail.com>
2024-07-04 17:13:28 -0300

    Initial commit

---
+++ tdd_project/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from tdd_project.core.config import settings
+
+
+class App(FastAPI):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(
+            *args, 
+            **kwargs, 
+            version="0.0.1", 
+            title=settings.PROJECT_NAME, 
+            # root_path=settings.ROOT_PATH
+        )
+
+
+app = App()
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello World"}
+
+# @app.get("/docs")
+# def read_docs():
+#     return {"message": "Docs Page"}
